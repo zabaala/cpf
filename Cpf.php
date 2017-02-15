b20767ff85e8abbbc11ab16a2448907f8f1bda81
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2017-02-15 19:57:04 -0300

    refactoring class

--- Cpf.php
+++ Cpf.php
@@ -148,67 +148,196 @@ class Cpf
    * Compoe o CPF com 3 blocos e dígito verificador e dois algarismos.
    * @return string
    */
-  private function compose(){
+class Cpf
+{
+    /**
+     * Numero do cpf a ser retornado.
+     * @var string
+     */
+    protected $cpf;
 
-    if ($this->isValidation) {
-      $this->cpf = substr($this->cpf, 0, strlen($this->cpf)-2);
+    /**
+     * Primeiro algaritimo do novo dígito verificador.
+     * @var mixed
+     */
+    protected $dv1;
+
+    /**
+     * Segundo algaritimo do novo dígito verificador.
+     * @var mixed
+     */
+    protected $dv2;
+
+    /**
+     * Verifica se a requisição é de validação ou criação de um CPF.
+     * @var boolean
+     */
+    protected $isValidation = false;
+
+    /**
+     * Método construtor.
+     * @param string $cpf
+     * @throws \Exception
+     */
+    public function __construct($cpf = '') {
+
+        if (!empty($cpf) && strlen($cpf) < 9) {
+            throw new \Exception("O CPF precisa possuir, no mínimo 9 caracteres.", 1);
+        }
+        $this->cpf = $cpf;
     }
 
-    return sprintf(
-      '%s-%u%u',
-      $this->generateFakeBlocks(),
-      $this->calculateDv1(),
-      $this->calculateDv2()
-    );
-  }
+    /**
+     * Retorna o CPF quando a descrição da classe for requisitada.
+     * @return string
+     */
+    public function __toString() {
+        return $this->get();
+    }
 
-  /**
-   * Cria dinamicamente um número de CPF válido.
-   * 
-   * @return string
-   */
-  public function create() {
-    return $this->cpf = $this->compose();
-  }
+    /**
+     * Considerando que o CPF é composto por 3 blocos de
+     * 3 digitos, separados por um ponto(.) e complementados por
+     * um dígito verificador, este método gera números randômicos para
+     * cada bloco de um CPF, caso o objetivo seja criar CPFs dinâmicos.
+     *
+     * @param  integer $digitos
+     * @return int
+     */
+    private function generateBlock($digitos = 3) {
 
-  /**
-   * Retorna o cpf.
-   * 
-   * @return string
-   */
-  public function getCpf() {
-    return $this->cpf;
-  }
+        $bloco = '';
+        for ($i=0; $i < $digitos; $i++) {
+            $bloco .= mt_rand(0, $digitos);
+        }
+        return $bloco;
+    }
 
-  /**
-   * Remove caracteres especiais da string, deixando somente números.
-   * @param  string $str 
-   * @return string
-   */
-  private function clean($str) {
-    return preg_replace('/[^0-9]/ism', '', $str);
-  }
+    /**
+     * Cria os primeiros nove dígitos do CPF.
+     *
+     * @return string
+     */
+    private function generateFakeBlocks() {
 
-  /**
-   * Remove caracteres especiais do CPF.
-   * @return string 
-   */
-  public function getCleanCpf() {
-    return $this->clean($this->cpf);
-  }
+        if(!empty($this->cpf)) {
+            return $this->getCpf();
+        }
+        $this->cpf = sprintf(
+            '%s.%s.%s',
+            $this->generateBlock(),
+            $this->generateBlock(),
+            $this->generateBlock()
+        );
+        return $this->cpf;
+    }
 
-  /**
-   * Efetua a validação de um número de CPF.
-   * @return bool
-   */
-  public function validate() {
+    /**
+     * Cria o primeiro algaritimo do digito verificador,
+     * Baseado no CPF disponível para cálculo.
+     *
+     * @return integer
+     */
+    private function calculateDv1() {
+        if (!empty($this->dv1)) {
+            return $this->dv1;
+        }
+        $cpf = $this->clean();
+        for($i=0, $j=10; $i<9; $i++, $j--) {
+            $arr[$i] = $cpf{$i} * $j;
+        }
+        $s = array_sum($arr);
+        $r = $s % 11;
+        if($r<2) {
+            $dv = 0;
+        } else {
+            $dv = 11 - $r;
+        }
+        return $this->dv1 = $dv;
+    }
 
-    $this->isValidation = true;
+    /**
+     * Cria o segundo algaritimo do digito verificador,
+     * Baseado no CPF disponível para cálculo.
+     *
+     * @return integer
+     */
+    private function calculateDv2() {
 
-    $tmpCpf   = $this->cpf; 
-    $testCpf  = $this->create($this->cpf);
+        $cpf = $this->clean() . $this->calculateDv1();
 
-    return $this->clean($tmpCpf) == $this->clean($testCpf);
-  }
+        for ($i=0, $j=11; $i<10; $i++, $j--) {
+            $arr[$i] = (int)substr($cpf, $i, 1) * $j;
+        }
+        $s = array_sum($arr);
+        $r = $s % 11;
+        if($r < 2) {
+            $dv = 0;
+        } else {
+            $dv = 11 - $r;
+        }
+        return $this->dv2 = $dv;
+    }
 
+    /**
+     * Compoe o CPF com 3 blocos e dígito verificador e dois algarismos.
+     * @return string
+     */
+    private function compose(){
+        if ($this->isValidation) {
+            $this->cpf = substr($this->cpf, 0, strlen($this->cpf)-2);
+        }
+        return sprintf(
+            '%s-%u%u',
+            $this->generateFakeBlocks(),
+            $this->calculateDv1(),
+            $this->calculateDv2()
+        );
+    }
+
+    /**
+     * Cria dinamicamente um número de CPF válido.
+     *
+     * @return string
+     */
+    public function create() {
+        return $this->cpf = $this->compose();
+    }
+
+    /**
+     * Retorna o cpf.
+     *
+     * @return string
+     */
+    public function get() {
+        return $this->cpf;
+    }
+
+    /**
+     * Remove caracteres especiais da string, deixando somente números.
+     * @param  string $str
+     * @return string
+     */
+    private function cleanify($str) {
+        return preg_replace('/[^0-9]/ism', '', $str);
+    }
+
+    /**
+     * Remove caracteres especiais do CPF.
+     * @return string
+     */
+    public function clean() {
+        return $this->cleanify($this->cpf);
+    }
+
+    /**
+     * Efetua a validação de um número de CPF.
+     * @return bool
+     */
+    public function validate() {
+        $this->isValidation = true;
+        $tmpCpf   = $this->cpf;
+        $testCpf  = $this->create();
+        return $this->cleanify($tmpCpf) == $this->cleanify($testCpf);
+    }
 }
